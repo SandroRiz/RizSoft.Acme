2d2c74dd064ba8e4dc13e411e2df5d5103c38399
Sandro Rizzetto <sandro@rizzetto.com>
2022-03-02 18:36:58 +0100

    ProductService.AddTag

--- RizSoft.Acme.Services/Services/ProductService.cs
+++ RizSoft.Acme.Services/Services/ProductService.cs
@@ -13,21 +13,34 @@ public class ProductService : BaseService<Product, int>
         using var ctx = CtxFactory.CreateDbContext();
         return await ctx.Products
             .Include(p => p.IdTags)
-            .Where( p => p.Id == id)
+            .Where(p => p.Id == id)
             .FirstOrDefaultAsync();
     }
 
+    public async Task<Product> AddTag(int idProduct, Tag tag)
+    {
+        using var ctx = CtxFactory.CreateDbContext();
+        var product = await ctx.Products.FindAsync(idProduct);
+        if (product != null)
+        {
+            product.IdTags.Add(tag);
+            await ctx.SaveChangesAsync();
+            return product;
+        }
+        else
+            return null;
+    }
 
     public async Task<List<Product>> ListActive()
     {
         using var ctx = CtxFactory.CreateDbContext();
         return await ctx.Products
-            .Include( p => p.Category)
+            .Include(p => p.Category)
             .Where(e => !e.Discontinued)
             .OrderBy(e => e.ProductName)
             .ToListAsync();
 
     }
 
-   
+
 }
